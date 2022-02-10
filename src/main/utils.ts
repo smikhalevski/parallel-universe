29e6797dc3772b452994ac6a4a009a4e7a2865fb
Savva Mikhalevski <smikhalevski@gmail.com>
2022-02-11 00:39:57 +0300

    Added code to error

--- src/main/utils.ts
+++ src/main/utils.ts
@@ -1,18 +1,19 @@
-function createError(name: string, message?: string): Error {
+function createError(name: string, code: number, message?: string): Error {
   if (typeof DOMException !== 'undefined') {
     return new DOMException(message, name);
   }
-  const error = new Error(message);
+  const error: any = new Error(message);
   error.name = name;
+  error.code = code;
   return error;
 }
 
 export function createAbortError(): Error {
-  return createError('AbortError');
+  return createError('AbortError', 20);
 }
 
 export function createTimeoutError(): Error {
-  return createError('TimeoutError');
+  return createError('TimeoutError', 23);
 }
 
 export function callOrGet<T, A extends unknown[]>(value: ((...args: A) => T) | T, ...args: A): T {
