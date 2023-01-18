9796c25ae6b92413982195ab5face90ca66300a9
smikhalevski <smikhalevski@gmail.com>
2023-01-18 19:43:08 +0300

    Moved isPromiseLike to utils

--- src/main/isPromiseLike.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Returns `true` is value has `then` property that is a function.
- */
-export function isPromiseLike<T>(value: any): value is PromiseLike<T> {
-  return value != null && typeof value === 'object' && typeof value.then === 'function';
-}
